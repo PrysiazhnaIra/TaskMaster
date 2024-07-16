7fdf3b754769aa7b631f925fcf93c2367eb842ea
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-07-16 14:02:47 +0300

    localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import "./App.css";
 import "modern-normalize";
 import initialTasks from "./data/tasks.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./components/Background/Background";
 import Filter from "./components/Filter/Filter";
 import Form from "./components/Form/Form";
 import TaskList from "./components/TaskList/TaskList";
 
 function App() {
-  const [currentColor, setCurrentColor] = useState("#90EE90");
+  // Ініціалізація tasks
+  const [tasks, setTasks] = useState(() => {
+    return JSON.parse(window.localStorage.getItem("tasks")) || initialTasks;
+  });
+
+  // Ініціалізація currentColor
+  const [currentColor, setCurrentColor] = useState(() => {
+    return window.localStorage.getItem("backgroundColor") || "#90EE90";
+  });
+
+  const [filter, setFilter] = useState("");
+
+  // Збереження tasks в localStorage при їх зміні
+  useEffect(() => {
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // Збереження currentColor в localStorage при його зміні
+  useEffect(() => {
+    window.localStorage.setItem("backgroundColor", currentColor);
+  }, [currentColor]);
 
   const handleChangeColor = (color) => {
     setCurrentColor(color);
   };
-
-  const [tasks, setTasks] = useState(initialTasks);
-  const [filter, setFilter] = useState("");
 
   const addTask = (newTask) => {
     setTasks((prev) => {
